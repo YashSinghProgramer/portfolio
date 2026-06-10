bd26f84e54d07e1198883bc244d77aa1a7480bde
YashSinghProgramer <yashsinghprogramer@gmail.com>
2026-06-10 10:33:21 +0530

    Update Card.jsx

--- src/contant/Project/Card.jsx
+++ src/contant/Project/Card.jsx
@@ -1,51 +1,50 @@
-import {useContext} from 'react'
-import { ThemeContext } from '../hero sec/ThemeContext.jsx'
-import kilogramlogo from './kilogram.png'
-const details=[
-   {
-        img:kilogramlogo,
-        name:"Kilogram",
-        desc:"A Instagram Clone UI, app using React and CSS.",
-        Link:"https://kilogramcom.vercel.app/",
-        btn1:"Visit",
-        btn2:"<Code />",
-        staus:"On Testing",
-        catagory:"Frontend",
-    },
-    
-]
+import { useContext } from "react";
+import { ThemeContext } from "../hero sec/ThemeContext.jsx";
+import kilogramlogo from "./kilogram.png";
+const details = [
+	{
+		img: kilogramlogo,
+		name: "Kilogram",
+		desc: "A Instagram Clone UI, app using HTML/CSS, React, Express.JS, MongoDB.",
+		Link: "https://kilogramcom.vercel.app/",
+		btn1: "Visit",
+		btn2: "<Code />",
+		staus: "On Testing",
+		catagory: "Frontend",
+	},
+];
 
 function Card({ selectedCategory }) {
-    
-    
- const {themeProject} = useContext(ThemeContext);
-// Map chalane se pehle array ko filter kiya
-  const filteredDetails = details.filter(item => {
-    if (selectedCategory === 'All') return true;
-    return item.catagory === selectedCategory;
-  });
-  return (
-    
-    <div className={themeProject.Cardcon} >
-        {/* <h1>We Are Working On Projects</h1> */}
-   {filteredDetails.map((item,index)=>(
+	const { themeProject } = useContext(ThemeContext);
+	// Map chalane se pehle array ko filter kiya
+	const filteredDetails = details.filter((item) => {
+		if (selectedCategory === "All") return true;
+		return item.catagory === selectedCategory;
+	});
+	return (
+		<div className={themeProject.Cardcon}>
+			{/* <h1>We Are Working On Projects</h1> */}
+			{filteredDetails.map((item, index) => (
+				<div className={themeProject.card} key={index}>
+					<img src={item.img} alt="Project Img" />
+					<h2>
+						{item.name}
+						<ul>
+							<li>{item.staus}</li>
+						</ul>
+					</h2>
+					<h4>{item.desc}</h4>
+					<div className={themeProject.btn}>
+						<a href={item.Link} target="_blank" rel="noopener noreferrer">
+							<button className={themeProject.btn1}>{item.btn1}</button>
+						</a>
 
-    <div className={themeProject.card} key={index}>
-
-    <img src={item.img} alt='Project Img'/>
-<h2>{item.name}<ul><li>{item.staus}</li></ul></h2>
-<h4>{item.desc}</h4>
-<div className={themeProject.btn}>
- <a href={item.Link} target="_blank" rel="noopener noreferrer">
-              <button className={themeProject.btn1}>{item.btn1}</button>
-            </a>
-    
-    <button className={themeProject.btn2}>{item.btn2}</button>
-</div>
-
-    </div> ))}
-   </div>
-  )
+						<button className={themeProject.btn2}>{item.btn2}</button>
+					</div>
+				</div>
+			))}
+		</div>
+	);
 }
 
-export default Card
+export default Card;
